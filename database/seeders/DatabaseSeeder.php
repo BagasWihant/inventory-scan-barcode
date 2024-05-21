c05936b33683d4a6e1e2e7a8ee2163ba72291fe4
BagasWihant <bagaswihant@gmail.com>
2024-05-21 14:31:47 +0700

    fix:Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Pallet;
 use App\Models\Product;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,13 +17,44 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        // User::factory()->create([
-        //     'name' => 'bagas',
-        //     'email' => 'bagas@mail.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'bagas',
+            'email' => 'bagas@mail.com',
+        ]);
+        
+        DB::table('pallets')->insertOrIgnore([
+            'pallet_barcode' => 'Y-01-00003',
+            'line' => 'CNC',
+            'pallet_serial' => 'asda',
+            'trucking_id' => 987,
+            'scanned_by' => 1,
+            'is_scanned' => 0,
+        ]);
+        $csvFile = public_path('data.csv');
 
-        Pallet::factory(6)->create();
-        Product::factory(120)->create();
+        if (!file_exists($csvFile) || !is_readable($csvFile)) {
+            $this->command->error('CSV file not found or not readable');
+            return;
+        }
+
+        if (($handle = fopen($csvFile, 'r')) !== false) {
+            
+            while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+                DB::table('products')->insert([
+                    'pallet_barcode' => $data[2],
+                    'material_no' => $data[0],
+                    'qty' => $data[1],
+                    'is_scanned' => 0,
+                ]);
+            }
+
+            fclose($handle);
+            $this->command->info('Data inserted successfully');
+        } else {
+            $this->command->error('Error reading CSV file');
+        }
+        // Pallet::factory(6)->create();
+        // Product::factory(120)->create();
 
     }
 }
