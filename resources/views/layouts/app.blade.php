33cb041501725b60d402b3c0802ec0ec9935ce5e
BagasWihant <bagaswihant@gmail.com>
2024-05-27 15:44:57 +0700

    fix: sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,11 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <!-- Scripts -->
     <script src="{{ asset('assets/jquery.js') }}"></script>
+    {{-- <script defer src="{{ asset('assets/cdn.min.js') }}"></script> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
+<body class="font-sans antialiased" x-data="{ sidebarOpen: false }">
 
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
 
@@ -25,9 +26,8 @@
             <div class="px-3 py-3 lg:px-5 lg:pl-3">
                 <div class="flex items-center justify-between">
                     <div class="flex items-center justify-start rtl:justify-end">
-                        <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar"
-                            aria-controls="logo-sidebar" type="button"
-                            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                        <button  @click="sidebarOpen = !sidebarOpen" type="button"
+                            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                             <span class="sr-only">Open sidebar</span>
                             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -35,10 +35,12 @@
                                     d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
                                 </path>
                             </svg>
+
                         </button>
                         <a href="#" class="flex ms-2 md:me-24">
                             <span
-                                class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Inventory Barcode</span>
+                                class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Inventory
+                                Barcode</span>
                         </a>
                     </div>
                     <div class="flex items-center">
@@ -111,22 +113,24 @@
                 </div>
             </div>
         </nav>
+        <div class="flex">
+            <aside :class="sidebarOpen ? 'left-0 w-72' : '-left-72 w-0'"
+                class="relative h-screen transition-all mt-16 duration-300 flex flex-col text-md font-semibold top-0 z-40 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+                aria-label="Sidebar">
+                
+                <div :class="sidebarOpen ? 'px-3' : '' " class="h-full py-4 overflow-y-auto bg-white dark:bg-gray-800">
+                    <ul class="space-y-2 font-medium text-sm">
+                        <x-sidebar.list-menu :menu="[]" />
+                    </ul>
+                </div>
+            </aside>
 
-        <aside id="logo-sidebar"
-            class="fixed top-0 left-0 z-40 w-52 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-            aria-label="Sidebar">
-            <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-                <ul class="space-y-2 font-medium text-sm">
-                    <x-sidebar.list-menu :menu="[]" />
-                </ul>
+            <div class="p-4 w-full mt-16">
+                <!-- Page Content -->
+                <main>
+                    {{ $slot }}
+                </main>
             </div>
-        </aside>
-
-        <div class="p-4 sm:ml-52 mt-16">
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
         </div>
 
     </div>
