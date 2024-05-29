f5cef509046718f7ec3429005afd8e14fa387af4
bagaswihant <bagaswihant@gmail.com>
2024-05-29 13:10:05 +0700

    fix: counter

--- app/Models/tempCounter.php
+++ app/Models/tempCounter.php
@@ -10,7 +10,7 @@ class tempCounter extends Model
     use HasFactory;
     public $timestamps = false;
     public $keyType = 'string';
-    protected $fillable= ['material','material_fix','userID','total','counter','sisa','palet','pax'];
+    protected $fillable= ['material','userID','total','counter','sisa','palet','pax'];
 
     
 }
