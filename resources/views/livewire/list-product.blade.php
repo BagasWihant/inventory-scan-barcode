4905ece00b5fd8fdcd30d8ebcc4ebce239a4764d
BagasWihant <bagaswihant@gmail.com>
2024-06-26 09:49:55 +0700

    add if reset scan

--- resources/views/livewire/list-product.blade.php
+++ resources/views/livewire/list-product.blade.php
@@ -172,10 +172,12 @@
                                 </th>
                                 <th scope="row"
                                     class="p-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <button wire:click="resetItem({{ json_encode([$v->material, $v->palet]) }})"
-                                        class="relative inline-flex items-center justify-center  overflow-hidden text-sm font-medium text-gray-900 rounded-lg p-1 group bg-gradient-to-br from-red-800 to-red-500 group-hover:from-red-900 group-hover:to-red-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                                        Reset
-                                    </button>
+                                    @if ($v->counter > 0)
+                                        <button wire:click="resetItem({{ json_encode([$v->material, $v->palet]) }})"
+                                            class="relative inline-flex items-center justify-center  overflow-hidden text-sm font-medium text-gray-900 rounded-lg p-1 group bg-gradient-to-br from-red-800 to-red-500 group-hover:from-red-900 group-hover:to-red-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                            Reset
+                                        </button>
+                                    @endif
                                 </th>
                             </tr>
                         @endforeach
