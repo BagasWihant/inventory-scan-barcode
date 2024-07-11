6ca3b04b4c5d453f4e1d68b309c81410f08040a5
BagasWihant <bagaswihant@gmail.com>
2024-07-11 09:30:27 +0700

    change to combobox

--- resources/views/livewire/material-registrasi.blade.php
+++ resources/views/livewire/material-registrasi.blade.php
@@ -3,17 +3,25 @@
 
     <div class="flex gap-5 justify-items-center">
 
-        <div class="w-full">
-            <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">PO
+        <div class="w-1/4">
+            <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Material No
             </label>
-            <input wire:model="searchMaterial" wire:keydown.debounce.300ms="materialType" type="text" id="produkBarcode"
-                @if ($materialDisable) disabled @endif autocomplete="off"
-                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
+            <select id="materialselect" style="width: 100%" wire:model="material"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full !p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose Material</option>
+                @foreach ($listMaterialComboBox as $p)
+                    <option value="{{ $p->matl_no }}">{{ $p->matl_no }}</option>
+                @endforeach
+            </select>
+
+            {{-- <input wire:model="searchMaterial" wire:keydown.debounce.300ms="materialType" type="text" id="produkBarcode"
+            @if ($materialDisable) disabled @endif autocomplete="off"
+            class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <div class="absolute">
                 <div class="py-3 text-center bg-green-100 text-green-700 rounded-lg" wire:loading.block
                     wire:target="materialType">Searching</div>
-                <div wire:loading.remove class="rounded-lg bg-slate-50 shadow">
+                <div wire:loading.remove class="rounded-lg bg-slate-50 shadow overflow-y-auto h-72">
 
                     @if (strlen($searchMaterial) >= 1 && $material != $searchMaterial)
                         @forelse ($listMaterialComboBox as $p)
@@ -25,7 +33,7 @@
                         @endforelse
                     @endif
                 </div>
-            </div>
+            </div> --}}
 
         </div>
         <div class="flex items-center">
@@ -70,7 +78,7 @@
                             {{ $d->material_no }}
                         </td>
                         <td class="px-6 py-1">
-                            <button type="button" id="btn-kembalikan" wire:click="hapus({{ $d->id }})"
+                            <button type="button" id="btn-kembalikan" onclick="hapus({{ $d->id }})"
                                 class="text-white bg-gradient-to-r from-red-500  to-red-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none transition-all focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-xl text-sm px-5 py-2.5 text-center me-2 mb-2">Hapus</button>
 
                         </td>
@@ -79,7 +87,23 @@
             </tbody>
         </table>
         {{ $listMaterialAdded->links() }}
-
+        <script>
+            function hapus(id) {
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        @this.deleteMaterial(id)
+                    }
+                })
+            }
+        </script>
     @endif
 </div>
 
@@ -93,6 +117,16 @@
                 showConfirmButton: false,
                 timerProgressBar: true,
             });
+        });
+        $('#materialselect').select2({
+            placeholder: "Material Code",
+            width: 'resolve',
+            tags: true
+        });
+        $('#materialselect').on('change', function(e) {
+            @this.material = e.target.value
+            // id = $('#ii').val();
+            // $('#ii').val('');
         });
     </script>
 @endscript
