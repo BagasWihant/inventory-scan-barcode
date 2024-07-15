7ed9e049f466a3299a317b011bcd760c4110b577
BagasWihant <bagaswihant@gmail.com>
2024-07-15 16:20:45 +0700

    fix new column material instock & abnormal

--- app/Models/abnormalMaterial.php
+++ app/Models/abnormalMaterial.php
@@ -9,5 +9,5 @@ class abnormalMaterial extends Model
 {
     use HasFactory;
     
-    protected $fillable= ['pallet_no','material_no','picking_qty','locate','trucking_id','user_id','status','kit_no','surat_jalan'];
+    protected $fillable= ['pallet_no','material_no','picking_qty','locate','trucking_id','user_id','status','kit_no','surat_jalan','line_c','setup_by'];
 }
