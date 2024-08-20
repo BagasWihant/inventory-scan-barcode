520e074dd7c8f78521f916504cd09f13d5bc7483
bagaswihant <bagaswihant@gmail.com>
2024-08-20 18:31:49 +0700

    fix menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import 'flowbite';
+import { initFlowbite } from 'flowbite';
 import Swal from 'sweetalert2';
 
- 
+document.addEventListener('livewire:navigated', () => {
+    initFlowbite();
+})
 window.Swal = Swal;
