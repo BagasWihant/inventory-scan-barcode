e5abc4300ce179ff3ebe8926dd31ef1c0daf4935
Bagas Wihantoro <41191500+BagasWihant@users.noreply.github.com>
2024-10-22 13:48:40 +0700

    [fix] view standar kerja

--- app/Livewire/Menu/StandarKerja.php
+++ app/Livewire/Menu/StandarKerja.php
@@ -6,19 +6,22 @@ use Livewire\Component;
 
 class StandarKerja extends Component
 {
-    public $urlPdf=null;
+    public $urlPdf = null, $active;
 
     public function showPdf($type) {
         switch ($type) {
             case '1':
+                $this->active = 1;
                 $this->urlPdf = asset('Sertifikat-AIPT-STMIK-Amikom-Surakarta.pdf');
                 break;
-            
+                
             case '2':
+                $this->active = 1;
                 $this->urlPdf = asset('Bagas Wihantoro.pdf');
                 break;
-            
+                    
             default:
+                $this->active = 99;
                 $this->urlPdf = null;
                 break;
         }    
--- resources/views/livewire/menu/standar-kerja.blade.php
+++ resources/views/livewire/menu/standar-kerja.blade.php
@@ -1,79 +1,29 @@
 <div class="">
 
-    <div class="flex justify-center rounded-md " role="group">
-        <button type="button" wire:click="showPdf(1)"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path
-                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-            </svg>
-            1
-        </button>
-        <button type="button" wire:click="showPdf(2)"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
-            </svg>
-            2
-        </button>
-        <button type="button" wire:click="showPdf(3)"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path
-                    d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
-                <path
-                    d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
-            </svg>
-            3
-        </button>
-        <button type="button" wire:click="showPdf(4)"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path
-                    d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
-                <path
-                    d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
-            </svg>
-            4
-        </button>
+    <div class="flex min-h-screen">
+        <ul
+            class="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+            <li>
+                <button wire:click="showPdf(1)"
+                    class="inline-flex items-center px-4 py-3 rounded-lg w-full @if ($active == 1) text-white bg-blue-700 @else text-gray-900 bg-gray-50 @endif  "
+                    aria-current="page">
+                    1
+                </button>
+            </li>
+            <li>
+                <button wire:click="showPdf(2)"
+                    class="inline-flex items-center px-4 py-3 rounded-lg w-full @if ($active == 2) text-white bg-blue-700 @else text-gray-900 bg-gray-50 @endif ">
+                    2
+                </button>
+            </li>
+        </ul>
+        <div class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
+
+            @if ($urlPdf)
+                <iframe src="{{ $urlPdf }}" frameborder="no" style="width:100%;min-height:90vh"></iframe>
+            @endif
+        </div>
     </div>
 
-    <div wire:loading.flex
-        class=" fixed z-30 bg-slate-900/60 dark:bg-slate-400/35 top-0 left-0 right-0 bottom-0 justify-center items-center h-screen border border-red-800"
-        wire:target="showPdf" aria-label="Loading..." role="status">
-        <svg class="h-20 w-20 animate-spin stroke-white " viewBox="0 0 256 256">
-            <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24">
-            </line>
-            <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24">
-            </line>
-            <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="24">
-            </line>
-        </svg>
-        <span class="text-4xl font-medium text-white">Loading...</span>
-    </div>
-    @if ($urlPdf)
-        <iframe src="{{ $urlPdf }}" frameborder="no" style="width:100%;min-height:90vh"></iframe>
-    @endif
+
 </div>
-
-
-
-
