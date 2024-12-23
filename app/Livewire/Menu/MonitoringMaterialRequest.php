a7889f0794b0c89126a43558dcd334d1e28ca5d7
bagaswihant <bagaswihant@gmail.com>
2024-12-23 22:29:49 +0700

    fixing: monitoring material

--- app/Livewire/Menu/MonitoringMaterialRequest.php
+++ app/Livewire/Menu/MonitoringMaterialRequest.php
@@ -13,33 +13,36 @@ class MonitoringMaterialRequest extends Component
 {
     use WithPagination, WithoutUrlPagination;
     public $time, $totalCount;
-    public function render()
-    {
-        $this->time =  now();
+    public $material;
 
+    public function refreshTable()
+    {
         $materialQuery = MaterialRequest::with('user')
             ->select([
                 DB::raw("STRING_AGG(material_no, ', ') WITHIN GROUP (ORDER BY material_no) AS material_no"),
+                DB::raw("STRING_AGG(user_request, ', ') WITHIN GROUP (ORDER BY material_no) AS user_request"),
                 DB::raw('MAX(created_at) as created_at'),
                 DB::raw('MAX(proses_date) as proses_date'),
                 DB::raw('count(transaksi_no) as total_varian'),
                 "transaksi_no",
-                'user_request',
                 'user_id',
                 'status',
             ])
             ->where('status', '!=', '-')
             ->where(DB::raw('CONVERT(date, created_at)'), $this->time->format('Y-m-d'))
             ->orderBy('created_at', 'desc')
-            ->groupBy('transaksi_no', 'user_request', 'user_id', 'status', 'created_at');
-
+            ->groupBy('transaksi_no', 'user_id', 'status', 'created_at');
 
         $this->totalCount = MaterialRequest::where('status', '!=', '-')
             ->where(DB::raw('CONVERT(date, created_at)'), $this->time->format('Y-m-d'))
             ->count();
 
-        $material = $materialQuery->paginate(25);
-
-        return view('livewire.menu.monitoring-material-request', compact('material'));
+        $this->material = $materialQuery->get();
+    }
+    public function render()
+    {
+        $this->time =  now();
+        $this->refreshTable();
+        return view('livewire.menu.monitoring-material-request');
     }
 }
