91d83b3e4c5f4c451d04011db03b14de5237badf
bagaswihant <bagaswihant@gmail.com>
2025-03-15 13:15:11 +0700

    fix judul biar oke

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Warehouse') }}</title>
+    {{-- <title>{{ config('app.name', 'Warehouse') }} </title> --}}
+    <title><x-sidebar.list-menu :menuList="false" /> </title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
