f597760a3f8f5b754ef7474ebae6706f836a27ad
bagaswihant <bagaswihant@gmail.com>
2025-03-22 10:43:28 +0700

    fix view edit eror

--- resources/views/livewire/receiving-s-i-w-s.blade.php
+++ resources/views/livewire/receiving-s-i-w-s.blade.php
@@ -193,8 +193,7 @@
                                                     if (result.isConfirmed) {
                                                         $dispatch('editQty', {
                                                             qty: result.value,
-                                                            material: data[1],
-                                                            palet: data[2]
+                                                            id: data[1]
                                                         })
                                                     } else if (result.isDenied) {
                                                         return Swal.fire({
@@ -214,7 +213,7 @@
                                                 class="relative inline-flex items-center justify-center  overflow-hidden text-sm font-medium text-gray-900 rounded-lg p-1 group bg-gradient-to-br from-red-800 to-red-500 group-hover:from-red-900 group-hover:to-red-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                                                 Reset
                                             </button>
-                                            <button @click="openModalQty(@js([$v->counter, $v->material, $v->palet]))"
+                                            <button @click="openModalQty(@js([$v->counter, $v->id]))"
                                                 class="relative inline-flex items-center justify-center  overflow-hidden text-sm font-medium text-gray-900 rounded-lg p-1 group bg-gradient-to-br from-yellow-800 to-yellow-500 group-hover:from-yellow-900 group-hover:to-yellow-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                                                 Edit
                                             </button>
