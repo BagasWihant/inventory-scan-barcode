7fe31a2c2eafcb2de7003f8c20789f32b226ff91
wihant <bagas@jogjacamp.com>
2025-08-26 10:45:43 +0700

    sesuaikan pdf

--- app/Exports/PackingExport.php
+++ app/Exports/PackingExport.php
@@ -10,7 +10,7 @@ class PackingExport
     {
         $mpdf = new Mpdf([
             'mode' => 'utf-8',
-            'format' => [58, 100], // 58mm x 100mm (bisa diganti panjangnya)
+            'format' => [80, 50], // 58mm x 100mm (bisa diganti panjangnya)
             'margin_left' => 0,
             'margin_right' => 0,
             'margin_top' => 2,
@@ -18,22 +18,23 @@ class PackingExport
         ]);
 
 
-        $html = '
+        $fraction = '
         <style>
+            @page { size: 80mm 50mm; margin: 0; }
             body { width: 100%; font-size: 8pt; font-family: sans-serif; }
             table {  border-collapse: collapse; margin: auto; }
             td { padding: 2px; }
         </style>
-        <div style="border:1px solid black;width:98%;margin:auto;margin-bottom:6px;">
+        <div style="border:1px solid black;width:98%;margin:auto;margin-bottom:6px;padding-top:6px">
             
             <table>
                 <tr>
-                    <td colspan="2"><h6 style="text-align:center;border:1px solid black;width:60px;margin-left:2px;">FRACTION</h6></td>
+                    <td colspan="2"><h6 style="text-align:center;border:1px solid black;width:100px;margin-left:2px;">FRACTION</h6></td>
                     <td rowspan="4" style="text-align:center;width:60px;"><b>' . $dataFraction['qr'] . '</b></td>
                 </tr>
                 <tr>
-                    <td style="text-align:left;width:60px;">Material No</td>
-                    <td style="text-align:left;width:70px;"><b>' . $dataFraction['material_no'] . '</b></td>
+                    <td style="text-align:left;width:80px;">Material No</td>
+                    <td style="text-align:left;width:100px;"><b>' . $dataFraction['material_no'] . '</b></td>
                 </tr>
                 <tr>
                     <td>Qty</td>
@@ -47,18 +48,18 @@ class PackingExport
             <div style="background: black;height:1px;width:100%;text-align:center">
                 <span style="color: white;text-align:center">Ditempel</span>
             </div>
-        </div>
+        </div>';
 
-        <div style="border:1px solid black;width:98%;margin:auto;margin-bottom:6px;">
+        $actual = '<div style="border:1px solid black;width:98%;margin:auto;margin-bottom:6px;padding-top:6px">
             
             <table>
                 <tr>
-                    <td colspan="2"><h6 style="text-align:center;border:1px solid black;width:60px;margin-left:2px;">ACTUAL SUPPLY</h6></td>
+                    <td colspan="2"><h6 style="text-align:center;border:1px solid black;width:120px;margin-left:2px;">ACTUAL SUPPLY</h6></td>
                     <td rowspan="5" style="text-align:center;width:60px;"><b>' . $dataActual['qr'] . '</b></td>
                 </tr>
                 <tr>
-                    <td style="text-align:left;width:60px;">Material No</td>
-                    <td style="text-align:left;width:70px;"><b>' . $dataFraction['material_no'] . '</b></td>
+                    <td style="text-align:left;width:80px;">Material No</td>
+                    <td style="text-align:left;width:130px;"><b>' . $dataFraction['material_no'] . '</b></td>
                 </tr>
                 <tr>
                     <td>Issue Qty</td>
@@ -77,7 +78,9 @@ class PackingExport
         ';
 
         $filePath = storage_path('app/public/packing/' . trim($fileName).'.pdf');
-        $mpdf->WriteHTML($html);
+        $mpdf->WriteHTML($fraction);
+        $mpdf->AddPage();
+        $mpdf->WriteHTML($actual);
         $mpdf->Output($filePath, 'F');
 
         // return filepath
